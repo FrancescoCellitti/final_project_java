820bb5851bb2271a5737a7ea2f1b134c7b433ca8
Francesco <francescocellitti@virgilio.it>
2025-12-02 16:30:47 +0100

    CROSSoRIGIN PER LOCALHOST5173

--- src/main/java/org/final_project_java/controller/FilmRestController.java
+++ src/main/java/org/final_project_java/controller/FilmRestController.java
@@ -7,12 +7,14 @@ import org.final_project_java.repository.FilmRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/films")
+@CrossOrigin(origins = "http://localhost:5173")
 public class FilmRestController {
     @Autowired
     private FilmRepository filmRepository;
